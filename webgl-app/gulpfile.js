4cf2a3aa313c0a302b162b9de6eeb793b5bd1d2b
Juan Camilo Gonzalez <info@juancgonzalez.com>
2016-06-07 18:14:53 +0800

    change structure and use babel

--- webgl-app/gulpfile.js
+++ webgl-app/gulpfile.js
@@ -2,38 +2,63 @@
 const gulp = require('gulp');
 const sync = require('browser-sync').create();
 const webpack = require('webpack-stream');
+const Uglify = require('webpack').optimize.UglifyJsPlugin;
 
-var wpOptions = {
+var webpackConfig = {
+  context: __dirname + '/src',
+  devtool: 'source-map',
+  entry: {
+    javascript: './js/index.js',
+    html: './index.html'
+  },
   output: {
-    filename: 'scripts.js'
+    filename: 'js/scripts.js',
+    path: __dirname + '/build'
   },
   module: {
     loaders: [
       {
         test: /\.(glsl|vs|fs)$/,
         loader: 'shader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]'
       }
     ],
   },
   glsl: {
-    chunkPath: __dirname + '/shaders'
-  }
+    chunkPath: __dirname + '/src/shaders'
+  },
+  plugins: [
+    // Remove comments from next lines to minify js
+
+    // new Uglify({
+    //   compress: {
+    //     warnings: false
+    //   }
+    // })
+  ]
 };
 
 gulp.task('serve', ['pack'], function() {
   sync.init({
     notify: false,
-    server: './server'
+    server: './build'
   });
 
-  gulp.watch(['./server/scripts.js', './server/shaders/**/*.*'], ['pack']);
-  gulp.watch('./server/*.html').on('change', sync.reload);
+  gulp.watch(['./src/**/*.js', './src/shaders/**/*.*', './src/*.html'], ['pack']);
 });
 
 gulp.task('pack', function() {
-  return gulp.src('server/scripts.js')
-    .pipe(webpack(wpOptions))
-    .pipe(gulp.dest('server/packed-js'))
+  return gulp.src('src/js/index.js')
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest('build'))
     .pipe(sync.stream());
 });
 
